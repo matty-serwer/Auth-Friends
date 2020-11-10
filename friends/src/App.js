a4dcf3b8e4e457fb251d230673000aa9ed58e0ee
Matthew Serwer <Matthew.Serwer@gmail.com>
2020-11-10 16:54:57 -0500

    Set up PrivateRoute and axiosWithAuth

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,9 +1,34 @@
-import './App.css';
+import React, { useState } from "react";
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+
+import "./App.css";
+
+import Login from './components/Login';
+import FriendsList from './components/FriendsList';
+import PrivateRoute from './components/PrivateRoute';
+
+import axiosWithAuth from './utils/axiosWithAuth';
 
 function App() {
+  const [isLoggedIn, setLoggedIn] = useState(false);
+
+  const logout = () => {
+
+  }
+
   return (
-    <div className="App">
-    </div>
+    <Router>
+      <div className='App'>
+        <Link to='/login'>Login</Link>
+        <Link to='/login' onClick={logout}>Logout</Link>
+        <Link to='/friendsList'>Friends List</Link>
+      </div>
+
+      <Switch>
+        <PrivateRoute exact path='/friendsList' component={FriendsList} />
+        <Route path="/login" component={Login} />
+      </Switch>
+    </Router>
   );
 }
 
---
+++ friends/src/components/FriendsList.js
@@ -0,0 +1,15 @@
+import React from 'react';
+// import Loader from 'react-loader-spinner';
+import axios from 'axios';
+
+class FriendsList extends React.Component {
+
+    render() {
+    return (
+        <>
+        </>
+    )
+    }
+}
+
+export default FriendsList;
